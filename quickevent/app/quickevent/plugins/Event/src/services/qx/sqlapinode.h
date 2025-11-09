b5e5d2a9d16fbeadc1db66d15a38772187e48c06
Fanda Vacek <fvacek@elektroline.cz>
2025-11-09 22:10:51 +0100

    QX SQL API node implemented

--- quickevent/app/quickevent/plugins/Event/src/services/qx/sqlapinode.h
+++ quickevent/app/quickevent/plugins/Event/src/services/qx/sqlapinode.h
@@ -16,9 +16,6 @@ class SqlApiNode : public QxNode
 	using Super = QxNode;
 public:
 	explicit SqlApiNode(shv::iotqt::node::ShvNode *parent);
-
-	static shv::coreqt::data::RpcSqlResult rpcSqlResultFromQuery(QSqlQuery &q);
-	static shv::chainpack::RpcValue::Map recordToMap(const QSqlRecord &rec);
 protected:
 	const std::vector<shv::chainpack::MetaMethod> &metaMethods() override;
 	shv::chainpack::RpcValue callMethod(const StringViewList &shv_path, const std::string &method, const shv::chainpack::RpcValue &params, const shv::chainpack::RpcValue &user_id) override;
