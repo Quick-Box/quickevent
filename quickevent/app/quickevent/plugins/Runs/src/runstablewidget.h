9e325a7563bbb8988f7c7ec7f2c9fa9b081a0027
Fanda Vacek <fvacek@elektroline.cz>
2025-11-10 23:10:32 +0100

    QxSQL API in QE

--- quickevent/app/quickevent/plugins/Runs/src/runstablewidget.h
+++ quickevent/app/quickevent/plugins/Runs/src/runstablewidget.h
@@ -7,6 +7,7 @@ class RunsTableItemDelegate;
 class CourseItemDelegate;
 
 namespace qf::gui { class TableView; }
+namespace Event::services::qx { struct QxRecChng; }
 
 namespace Ui {
 class RunsTableWidget;
@@ -36,6 +37,8 @@ private:
 	void onCustomContextMenuRequest(const QPoint &pos);
 	void onTableViewSqlException(const QString &what, const QString &where, const QString &stack_trace);
 	void onBadTableDataInput(const QString &message);
+
+	void onQxRecChng(const Event::services::qx::QxRecChng &chng);
 private:
 	Ui::RunsTableWidget *ui;
 	RunsTableModel *m_runsModel;
